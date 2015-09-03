c6c8fad14e3bcb0373801646abd0c624999d58f2
Stephen Way <way.stephen@gmail.com>
2015-09-03 13:59:37 -0700

    Fixes based on Foundation patch merge

--- _harp/gulpfile.js
+++ _harp/gulpfile.js
@@ -23,16 +23,16 @@ gulp.task('serve', function () {
     /**
      * Watch for scss changes, tell BrowserSync to refresh main.css
      */
-    gulp.watch('css/**/*.scss', function () {
-      reload('css/main.css', {stream: true});
+    gulp.watch(['public/**/*.scss'], function () {
+      reload('public/css/main.css', {stream: true});
     });
-    gulp.watch('js/**/*.js', function () {
-      reload('js/main.js', {stream: true});
+    gulp.watch('public/**/*.js', function () {
+      reload('public/js/main.js', {stream: true});
     });
     /**
      * Watch for all other changes, reload the whole page
      */
-    gulp.watch(['**/*.ejs', '**/*.md'], function () {
+    gulp.watch(['public/**/*.ejs', 'public/**/*.md', 'public/**/*.jade'], function () {
       reload();
     });
   });
